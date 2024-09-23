b5feffb6283701c12f7dc65acade028eff87d599
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2024-09-23 10:31:11 +0800

    baikin navbar

--- POSTTEST 3/script/script.js
+++ POSTTEST 3/script/script.js
@@ -1,5 +1,6 @@
 const hamburgerToggle = document.querySelector('.nav-hamburger input');
 const nav = document.querySelector('.nav-container ul')
+const navbar = document.querySelector('.navbar')
 
 hamburgerToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
