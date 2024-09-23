2aeeeaf882a9899addf9f584c6c03e78accd67f4
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2024-09-23 14:43:01 +0800

    collection dah jadi

--- POSTTEST 3/script/script.js
+++ POSTTEST 3/script/script.js
@@ -1,7 +1,11 @@
 const hamburgerToggle = document.querySelector('.nav-hamburger input');
-const nav = document.querySelector('.nav-container ul')
-const navbar = document.querySelector('.navbar')
+const nav = document.querySelector('.nav-container ul');
+const button = document.querySelector('#collection .collection-button button')
+
 
 hamburgerToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
+})
+button.addEventListener('click', function(){
+    alert("Halaman belum tersedia!!!")
 })
