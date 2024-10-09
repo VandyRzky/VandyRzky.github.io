963958cb5c5d21c9968140428755342a0e56ade1
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2024-10-09 22:15:17 +0800

    posttest 5

--- POSTTEST 5/kameradb.sql
+++ POSTTEST 5/kameradb.sql
@@ -1,9 +1,69 @@
-create table kamera(
-id int(5) not null,
-nama varchar(255) not null,
-deskripsi varchar(255) not null,
-foto varchar(255) not null,
-primary key(id)
-)auto_increment = 10;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 09, 2024 at 02:12 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
-alter table kamera modify id int (5) not null auto_increment, auto_increment=10;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kameradb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kamera`
+--
+
+CREATE TABLE `kamera` (
+  `id` int NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kamera`
+--
+
+INSERT INTO `kamera` (`id`, `nama`, `deskripsi`, `foto`) VALUES
+(10, 'aaa', 'aaa', 'kamera-1.jpg'),
+(11, 'bbb', 'bbbb', 'kamera-2.jpg'),
+(12, 'ccccc', 'cccccc', 'kamera-3.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kamera`
+--
+ALTER TABLE `kamera`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kamera`
+--
+ALTER TABLE `kamera`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
